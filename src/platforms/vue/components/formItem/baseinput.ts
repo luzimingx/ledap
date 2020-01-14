15d4f85c292c7b6e8244070248c7b1f3eb65b2da
luziming <luzimingx@gmail.com>
2020-01-14 19:01:16 +0800

    核对一遍完成

--- src/platforms/vue/components/formItem/baseinput.ts
+++ src/platforms/vue/components/formItem/baseinput.ts
@@ -32,6 +32,12 @@ export default {
             return this.model.getValidatorData(this.attr, 'string', 'max') || this.maxlength;
         }
     },
-    template: `<component :is="tag" :name="attr" :value="model[attr]" :placeholder="model.getAttributeHint(attr)" v-on="inputListeners" :maxlength="cMaxlength" v-bind="$attrs"></component>
+    template: `<div><template v-if="tag !== 'textarea'">
+            <input :name="attr" :value="model[attr]" :placeholder="model.getAttributeHint(attr)" v-on="inputListeners" v-bind="$attrs" :maxlength="cMaxlength">
+        </template>
+        <template v-else>
+            <textarea :name="attr" :value="model[attr]" :placeholder="model.getAttributeHint(attr)" v-on="inputListeners" v-bind="$attrs" :maxlength="cMaxlength">
+            </textarea>
+        </template></div>
     `,
 };
